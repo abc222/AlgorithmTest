3d29a84c7093517a3863ba7b06c5f1c37b855e45
wangyuchao <wangyuchao@do-global.com>
2020-03-28 13:42:38 +0800

    添加栈与队列互相实现

---
+++ src/queueandstack/QueueWithTwoStacks.java
@@ -0,0 +1,39 @@
+package queueandstack;
+
+import java.util.Stack;
+
+public class QueueWithTwoStacks {
+
+    public static void main(String[] args) {
+        QueueWithTwoStacks queueWithTwoStacks = new QueueWithTwoStacks();
+        Queue queue = queueWithTwoStacks.new  Queue();
+        queue.push(1);
+        queue.push(2);
+        System.out.println(queue.pop());
+        queue.push(3);
+        System.out.println(queue.pop());
+        System.out.println(queue.pop());
+    }
+
+    class Queue {
+        Stack<Integer> stack1 = new Stack<>();
+        Stack<Integer> stack2 = new Stack<>();
+
+        public void push(int v) {
+            stack1.push(v);
+        }
+
+        public int pop() {
+            if (stack2.isEmpty()) {
+                if (stack1.isEmpty())
+                    throw new RuntimeException("队列为空！");
+                else {
+                    while (!stack1.isEmpty()) {
+                        stack2.push(stack1.pop());
+                    }
+                }
+            }
+            return stack2.pop();
+        }
+    }
+}
---
+++ src/queueandstack/StackWithTwoQueues.java
@@ -0,0 +1,57 @@
+package queueandstack;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class StackWithTwoQueues {
+
+    Queue<Integer> queue1 = new ArrayDeque<>();
+    Queue<Integer> queue2 = new ArrayDeque<>();
+
+    public void push(int v) {
+        if (queue1.isEmpty() && queue2.isEmpty()) {
+            queue1.add(v);
+            return;
+        }
+        if (queue1.isEmpty()) {
+            queue2.add(v);
+            return;
+        }
+        if (queue2.isEmpty()) {
+            queue1.add(v);
+            return;
+        }
+    }
+
+    public int pop() {
+        if (queue1.isEmpty() && queue2.isEmpty()) {
+            throw new RuntimeException("栈为空！");
+        }
+        if (queue1.isEmpty()) {
+            while (queue2.size() > 1) {
+                queue1.add(queue2.poll());
+            }
+            return queue2.poll();
+        }
+        if (queue2.isEmpty()) {
+            while (queue1.size() > 1) {
+                queue2.add(queue1.poll());
+            }
+            return queue1.poll();
+        }
+        return (Integer)null;
+    }
+
+    public static void main(String[] args) {
+        StackWithTwoQueues qs = new StackWithTwoQueues();
+        qs.push(2);
+        qs.push(4);
+        qs.push(7);
+        qs.push(5);
+        System.out.println(qs.pop());
+        System.out.println(qs.pop());
+
+        qs.push(1);
+        System.out.println(qs.pop());
+    }
+}
